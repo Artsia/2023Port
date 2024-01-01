4e72af0c2eb6f938fa60b8f156287c2b68cdbeef
Phillip Kataswa <tomphillip756@gmail.com>
2023-12-31 22:33:34 -0500

    add rel="noreferrer"

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -28,10 +28,10 @@ function App() {
             <div className='cert'>
               <h3>Certifications</h3>
               <ul>
-                <li><a href="https://www.credly.com/badges/95236fa3-9eb5-4662-b378-b01772c3b434/linked_in_profile" target="_blank">AWS Certified Cloud Practitioner</a></li>
-                <li><a href="https://www.hackerrank.com/certificates/00ac2c061cee" target="_blank">MySQL</a></li>
-                <li><a href="https://www.credly.com/badges/30d2dbd4-aff5-412b-867a-0c873f1ce85d/linked_in_profile" target="_blank">Web Development Fundamentals</a></li>
-                <li><a href="https://www.freecodecamp.org/certification/Phillipkataswa/responsive-web-design" target="_blank">Responsive Web Design</a></li>
+                <li><a href="https://www.credly.com/badges/95236fa3-9eb5-4662-b378-b01772c3b434/linked_in_profile" rel="noreferrer" target="_blank">AWS Certified Cloud Practitioner</a></li>
+                <li><a href="https://www.hackerrank.com/certificates/00ac2c061cee" rel="noreferrer" target="_blank">MySQL</a></li>
+                <li><a href="https://www.credly.com/badges/30d2dbd4-aff5-412b-867a-0c873f1ce85d/linked_in_profile" rel="noreferrer" target="_blank">Web Development Fundamentals</a></li>
+                <li><a href="https://www.freecodecamp.org/certification/Phillipkataswa/responsive-web-design" rel="noreferrer" target="_blank">Responsive Web Design</a></li>
               </ul>
 
 
