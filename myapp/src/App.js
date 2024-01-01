2a69f100f1c1c75c85d4f6f40fe0f99d39c107ca
Phillip Kataswa <tomphillip756@gmail.com>
2023-12-31 22:27:06 -0500

    commit

---
+++ myapp/src/App.js
@@ -0,0 +1,106 @@
+
+function App() {
+  return (
+    <div className="App">
+
+    
+      <div className="intro">
+
+        <div className='first-container'>
+        
+          <div id='myName'>
+              <h1>Phillip Kataswa</h1>
+          </div>
+
+          <div id="skills-cert">
+
+            <div className='skills'>
+              <h3>Skills</h3>
+              <ul>
+                <li>Programming Languages: Java, Python, JavaScript</li>
+                <li>Libraries/Frameworks: React</li>
+                <li>Tools / Platforms: Visual Studio, Notepad++</li>
+                <li>Databases: Mysql, PostgreSQL, MongoDB</li>
+              </ul>
+
+            </div>
+
+            <div className='cert'>
+              <h3>Certifications</h3>
+              <ul>
+                <li><a href="https://www.credly.com/badges/95236fa3-9eb5-4662-b378-b01772c3b434/linked_in_profile" target="_blank">AWS Certified Cloud Practitioner</a></li>
+                <li><a href="https://www.hackerrank.com/certificates/00ac2c061cee" target="_blank">MySQL</a></li>
+                <li><a href="https://www.credly.com/badges/30d2dbd4-aff5-412b-867a-0c873f1ce85d/linked_in_profile" target="_blank">Web Development Fundamentals</a></li>
+                <li><a href="https://www.freecodecamp.org/certification/Phillipkataswa/responsive-web-design" target="_blank">Responsive Web Design</a></li>
+              </ul>
+
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="projects">
+        
+      <div id='plinks'>{/**300-317px height by 817 */}
+
+        <div className='blackCirlce'>GitHub</div>
+        <div className='blackCirlce'>Likedin</div>
+        <div className='blackCirlce'>email</div>
+  
+
+      </div>
+
+      {/**209 by 871 */}
+
+      <div className='p-Grid'>
+        <div className='pPic'>
+          {/**w=296px h=100% */}
+
+        </div>
+        <div className='pText-Repo'>
+          <p>t</p>
+          <p><a>Repo</a></p>
+        </div>
+      </div>
+
+      <div className='p-Grid'>
+        <div className='pPic'>
+          {/**w=296px h=100% */}
+
+        </div>
+        <div className='pText-Repo'>
+          <p>t</p>
+          <p><a>Repo</a></p>
+        </div>
+      </div>
+
+      <div className='p-Grid'>
+        <div className='pPic'>
+          {/**w=296px h=100% */}
+
+        </div>
+        <div className='pText-Repo'>
+          <p>t</p>
+          <p><a>Repo</a></p>
+        </div>
+      </div>
+      
+
+
+
+
+
+
+      </div>
+
+      
+     
+    </div>
+  );
+}
+
+export default App;
